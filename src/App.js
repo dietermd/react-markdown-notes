5b90d88e8ec30cf02b2bee0596f954915806ebc7
Dieter Mutiz <dieter_md@hotmail.com>
2023-05-01 01:11:10 +0000

    Using local storage

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import Sidebar from "./components/Sidebar";
 import Editor from "./components/Editor";
 
 export default function App() {
-  const [notes, setNotes] = React.useState([]);
+  const [notes, setNotes] = React.useState(
+    JSON.parse(localStorage.getItem("notes")) || []
+  );
   const [currentNoteId, setCurrentNoteId] = React.useState(
     (notes[0] && notes[0].id) || ""
   );
+
+  React.useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   function createNewNote() {
     const newNote = {
